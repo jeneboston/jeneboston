4eadb27fbec960911ff38de4065fd191b49d4fd1
jeneboston <smilemusik2013@gmail.com>
2023-10-08 23:54:41 +0300

    Первый коммит с моим проектом

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+d
+4src/main/java/de/ait/homework34/StringProcessor.java,3\3\33983f0d8a3f078ee93391a1781942327e208dfa
---
+++ src/main/java/de/ait/homework34/StringProcessor.java
@@ -0,0 +1,33 @@
+package de.ait.homework34;
+
+public class StringProcessor {
+    public String concatenate(String str1, String str2) {
+        return str1 + str2;
+    }
+
+    public int getLength(String str) {
+        return str.length();
+    }
+
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        String reversed = reverse(str);
+        return str.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+        StringProcessor processor = new StringProcessor();
+
+        // Example usage
+        String str1 = "hello";
+        String str2 = "world";
+
+        System.out.println("Concatenated string: " + processor.concatenate(str1, str2));
+        System.out.println("Length of string: " + processor.getLength(str1));
+        System.out.println("Reversed string: " + processor.reverse(str1));
+        System.out.println("Is 'hello' a palindrome? " + processor.isPalindrome("hello"));
+    }
+}
