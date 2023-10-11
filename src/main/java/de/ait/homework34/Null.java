d31c06e6c86c7a8e202bf4a9408131fe40dd5e00
jeneboston <smilemusik2013@gmail.com>
2023-10-11 19:34:11 +0300

    new folder

---
+++ src/main/java/de/ait/homework34/Null.java
@@ -0,0 +1,5 @@
+package de.ait.homework34;
+
+public class Null {
+}
+    git add .
---
+++ src/main/java/de/ait/homework35/CalculatorSim.java
@@ -0,0 +1,20 @@
+package de.ait.homework35;
+
+public class CalculatorSim {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            return -1;  // Результат деления на ноль
+        } else {
+            return a / b;
+        }
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
